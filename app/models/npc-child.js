82e4b745b82057b4bfb8f1d2a108c4c902920dd1
pipobizelli <pipo.bizelli@gmail.com>
2013-10-26 16:51:35 -0200

    alteracao na classe child

--- app/models/npc-child.js
+++ app/models/npc-child.js
@@ -1,8 +1,8 @@
-var NpcController = function(hp, npc_x, npc_y, game_id){
+var NpcController = function(hp, npc_tile_x, npc_tile_y, game_id){
 	var _npc = {},
 			_hp = hp,
-			_npc_x = npc_x,
-			_npc_y = npc_y,
+			_npc_tile_x = npc_tile_x,
+			_npc_tile_y = npc_tile_y,
 			_attackable = false,
 			_game_id = game_id 
 
@@ -20,10 +20,10 @@ var NpcController = function(hp, npc_x, npc_y, game_id){
 			return _hp;
 		},
 		getNpcX : function(){
-			return _npc_x;
+			return _npc_tile_x;
 		},
 		getNpcY : function(){
-			return _npc_y;
+			return _npc_tile_y;
 		},
 		getGameId : function(){
 			return _game_id;
