ee4843f2027fdeea985b0b464ab3b0dbad282a07
pipobizelli <pipo.bizelli@gmail.com>
2013-10-26 05:29:45 -0200

    times

--- app/models/game.js
+++ app/models/game.js
@@ -25,22 +25,22 @@ var Game = function(id, map_id){
 		getTeamB : function(){
 			return _team_b;
 		},
-		addNpctoTeam : function(npc, team_id){
+		addNpcToTeam : function(npc, team_id){
 			if(team_id == 'A'){
 				_team_a.push(npc);
 			}else{
 				_team_b.push(npc);
 			}
 		},
-		removeNpcfromTeam : function(npc, team_id){
+		removeNpcFromTeam : function(npc, team_id){
+			var team = [];
 			if(team_id == 'A'){
-				var team = _.without(_team_a, npc);
+				team = _.without(_team_a, npc);
 				_team_a.push(team);
 			}else{
-				var team = _.without(_team_b, npc);
+				team = _.without(_team_b, npc);
 				_team_b.push(team);
 			}
-
 		}
 	}
 }
