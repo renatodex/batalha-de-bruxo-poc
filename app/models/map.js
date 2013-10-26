d72f4a3e1c15d044feaf287fa042d89d67136af4
renatodex <renatodex@gmail.com>
2013-10-26 13:50:53 -0200

    Added formula to map

--- app/models/map.js
+++ app/models/map.js
@@ -1,5 +1,8 @@
 var map = function(width, height, tile_height, tile_width, sprite, tile_data ){
 
+	//function getY(tile_post) { return parseInt((tile_post-1)/tiles_per_line) * tile_width }
+	//function getX(tile_pos) { ((tile_pos-1)%tiles_per_line)*tile_width }
+
 	return{
 
 	}
