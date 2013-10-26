e1fa4cdd33c76073445db08e182ac3b1fbd7ae81
renatodex <renatodex@gmail.com>
2013-10-26 17:36:00 -0200

    Added render method to Canvas

--- app/models/canvas/npc-child.js
+++ app/models/canvas/npc-child.js
@@ -1,5 +1,12 @@
 var CanvasNpcChild = function() {	
+	
+	var _render = function(npc_child) {
+		var x = npc_child.getNpcTileX();
+		var y = npc_child.getNpcTileY();
+		console.log('NPC Rendered!', x, y);
+	}
+	
 	return {
-		
+		render:_render
 	}
 }();
