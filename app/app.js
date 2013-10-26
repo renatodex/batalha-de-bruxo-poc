333b8299ee578cc4232687d3d5bfa01e99bfa59e
renatodex <renatodex@gmail.com>
2013-10-26 18:11:00 -0200

    Added app js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
 var App = function() {
-	
 	var _init = function(canvas_id, callback) {
 		_callback = callback || function() {};
 		stage = new createjs.Stage(canvas_id);
@@ -8,24 +7,27 @@ var App = function() {
 		w = stage.canvas.width;
 		h = stage.canvas.height;
 
-		manifest = [
+		/*manifest = [
 			{src:"assets/runningGrant.png", id:"grant"},
 			{src:"assets/sky.png", id:"sky"},
 			{src:"assets/ground.png", id:"ground"},
 			{src:"assets/parallaxHill1.png", id:"hill"},
 			{src:"assets/parallaxHill2.png", id:"hill2"}
-		];
+		];*/
 
 		loader = new createjs.LoadQueue(false);
-		loader.addEventListener("complete", handleComplete);
-		loader.loadManifest(manifest);		
+		loader.addEventListener("complete", callback);
+		loader.loadManifest([{id:"image", src:"assets/images/hero_sprite.png"}]);
+		loader.load();
 	}
 	
 	return {
-		init : _init;
+		init : _init
 	}
 }();
 
 App.init('alcides', function() {
-	ControllerNpcChild.render()
+	var game = FacadeGame.createGame([1,2,3,4], 40);
+	var npc_child = game.getTeamA()[0];
+	ControllerNpcChild.render(npc_child, 100, 101)
 });
