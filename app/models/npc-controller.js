4034e17cb3ede355dbfb40f72ce56dfb9e05b807
pipobizelli <pipo.bizelli@gmail.com>
2013-10-26 04:52:09 -0200

    Add de classes

---
+++ app/models/npc-controller.js
@@ -0,0 +1,26 @@
+var NpcController = function(vida_atual, npc_x, npc_y, game_id){
+	var _vida_atual = vida_atual,
+	_npc_x = x,
+	_npc_y = y,
+	_attackable = false,
+	_game_id = game_id 
+
+	return{
+		getVidaAtual : function(){
+			return ;
+		},
+		getNpcX : function(){
+			return ;
+		},
+		getNpcY : function(){
+			return ;
+		},
+		getGameId : function(){
+			return ;
+		},
+		setAttackable : function(attackable){
+			_attackable = attackable;
+
+		},
+	}
+}
