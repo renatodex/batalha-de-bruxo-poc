e3ec3486e27182e36958a55ff3778ebb9a39d7db
pipobizelli <pipo.bizelli@gmail.com>
2013-10-26 17:28:46 -0200

    metodo envoca o render do canvas

--- app/controllers/npc-child-controller.js
+++ app/controllers/npc-child-controller.js
@@ -1,8 +1,16 @@
 var NpcChildController = function(){
 	var _render = function(npc, x, y){
-		
-	}
+		FacadeNpcChild.isValidMove(npc, function(valid){
+			if(valid){
+				npc.setNpcTileX(x);
+				npc.setNpcTileY(y);
 
+				CanvasNpcChild.render(npc);
+			}else{
+				return false;
+			}
+		});
+	}
 	return{
 		render : _render
 	}
